b0a4d9746f69b38dd418dd3196befbc7fcb76374
PolinaScrbbs <polinascaraboobs@gmail.com>
2024-04-14 20:02:20 +0300

    Initial Commit

---
+++ setup_and_run.ps1
@@ -0,0 +1,21 @@
+python -m venv .venv
+
+$activateScript = ".\.venv\Scripts\Activate.ps1"
+if (Test-Path $activateScript) {
+    & $activateScript
+} else {
+    Write-Host "Не удалось найти скрипт активации виртуального окружения."
+    exit 1
+}
+
+pip install -r requirements.txt
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py loaddata fixtures/Roles.json
+
+python manage.py createsuperuser
+
+python manage.py runserver
